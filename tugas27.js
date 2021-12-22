0fe451fbe669b1828e8be6bab1a9b57b632ee8d3
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-22 13:02:17 +0700

    Pengumpulan Tugas 27 JS - NIOMIC

---
+++ tugas27.js
@@ -0,0 +1,17 @@
+const a = ["No", "rumah saya", 30, "A"];
+console.log(a.join(" "));
+
+const b = ["Jalan pangeran Tirtayasa No", 70];
+console.log(b.join(" "));
+
+console.log("\n==============================================");
+
+const c = "No";
+const d = "rumah saya";
+const e = 30;
+const f = "A";
+console.log(c, d, String(e) + f);
+
+const g = "Jalan pangeran Tirtayasa";
+const h = 70;
+console.log(g, String(h));
